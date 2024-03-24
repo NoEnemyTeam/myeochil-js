5124dae9f8057c830156226134531f30c09d75ad
Hyun-jin Kim <rladhkdwls520@naver.com>
2024-03-24 19:51:59 +0900

    Init: Development environment settings

---
+++ dist/cjs/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const hello = (name) => {
+    return `hello ${name}`;
+};
+exports.hello = hello;
