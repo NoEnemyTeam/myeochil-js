80dd84c5527fdde21b8e841b42af46f0acdaaa21
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2024-04-17 11:50:27 +0900

    Feat: add calDaysBetween

---
+++ src/calDate/calDaysBetween.ts
@@ -0,0 +1,10 @@
+import { parseISO, differenceInCalendarDays } from 'date-fns';
+
+export const calDaysBetween = (dateStr1: string, dateStr2: string): number => {
+  const date1 = parseISO(dateStr1);
+  const date2 = parseISO(dateStr2);
+  
+  const days = differenceInCalendarDays(date2, date1);
+
+  return Math.abs(days);
+}
