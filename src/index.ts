bef8286e67cc1103b426e61a9b0c19b8a58e8c72
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2024-05-24 17:51:58 +0900

    Docs: del test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-export const hello = (name: string): string => {
-    return `hello ${name}`;
-}
-
 export { calDaysBetween } from './calDate/calDaysBetween'
 export { sortDates } from './calDate/sortDate'
