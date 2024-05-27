0d0174dbd57e1f9ec911c38e0cb9cabb10039aec
hyunoi <nhe0622@gmail.com>
2024-05-28 02:35:53 +0900

    fix: changed index.js

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,12 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidDate = void 0;
+exports.getFutureWeekday = exports.getFutureDate = exports.isLeapYear = exports.isValidDate = void 0;
 var isValidDate_1 = require("./isValidDate");
 Object.defineProperty(exports, "isValidDate", { enumerable: true, get: function () { return isValidDate_1.isValidDate; } });
-exports.isLeapYear = void 0;
 var isLeapYear_1 = require("./isLeapYear");
 Object.defineProperty(exports, "isLeapYear", { enumerable: true, get: function () { return isLeapYear_1.isLeapYear; } });
-exports.getFutureWeekday = exports.getFutureDate = void 0;
 var getFutureDate_1 = require("./getFuture/getFutureDate");
 Object.defineProperty(exports, "getFutureDate", { enumerable: true, get: function () { return getFutureDate_1.getFutureDate; } });
 var getFutureWeekday_1 = require("./getFuture/getFutureWeekday");
--- dist/mjs/index.js
+++ dist/mjs/index.js
@@ -1,4 +1,4 @@
 export { isValidDate } from "./isValidDate";
 export { isLeapYear } from "./isLeapYear";
 export { getFutureDate } from './getFuture/getFutureDate';
-export { getFutureWeekday } from './getFuture/getFutureWeekday'
+export { getFutureWeekday } from './getFuture/getFutureWeekday';
