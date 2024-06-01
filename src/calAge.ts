6c9f3028ce4e48723a78b8dbde3dd59db3a18de5
Journeybongbong <77794756+Journeybongbong@users.noreply.github.com>
2024-06-01 23:52:36 +0900

    Docs: add error messages

--- src/calAge.ts
+++ src/calAge.ts
@@ -1,9 +1,15 @@
+import { isValidDate } from "./isValidDate";
+
 export function calAge(birthDate: string, referenceDate: string): number | string {
   const birth = new Date(birthDate);
   const reference = new Date(referenceDate);
 
+  if (!isValidDate(birth) || !isValidDate(reference)) {
+    throw new Error("Error: Enter a valid date that exists in the calendar");
+  }
+
   if (birth.getTime() > reference.getTime()) {
-    return "Error: ReferenceDate is earlier than birthDate.";
+    throw new Error("Error: ReferenceDate is earlier than birthDate.");
   }
 
   let age = reference.getFullYear() - birth.getFullYear();
--- src/calDate/calDaysBetween.ts
+++ src/calDate/calDaysBetween.ts
@@ -1,9 +1,13 @@
 import { parseISO, differenceInCalendarDays } from 'date-fns';
+import { isValidDate } from "../isValidDate";
 
 export function calDaysBetween(dateStr1: string, dateStr2: string): number {
+  if (!isValidDate(dateStr1) || !isValidDate(dateStr2)) {
+    throw new Error("Error: Enter a valid date that exists in the calendar");
+  }
   const date1 = parseISO(dateStr1);
   const date2 = parseISO(dateStr2);
-  
+
   const days = differenceInCalendarDays(date2, date1);
 
   return Math.abs(days);
