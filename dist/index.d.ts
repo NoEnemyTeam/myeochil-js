9db12d0839f10e9b6ab2b3884e907d28315dfb42
hyunoi <nhe0622@gmail.com>
2024-06-02 23:17:08 +0900

    feat: version upgrade

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,17 @@
-export declare const hello: (name: string) => string;
+export { isValidDate } from "./isValidDate";
+export { isLeapYear } from "./isLeapYear";
+export { getFutureDate } from './getFuture/getFutureDate';
+export { getFutureWeekday } from './getFuture/getFutureWeekday';
+export { calAge } from './calAge';
+export { calDaysBetween } from './calDate/calDaysBetween';
+export { sortDates } from './calDate/sortDate';
+export { addSchedule } from './schedules/addSchedule';
+export { addWeekSchedule } from './schedules/addSchedule';
+export { addIntervalSchedule } from './schedules/addSchedule';
+export { getSchedules } from './schedules/scheduleManage';
+export { deleteSchedule } from './schedules/scheduleManage';
+export { printSchedules } from './schedules/scheduleManage';
+export { toLunar, toSolar } from "./convertLunarSolar/convertDate";
+export { formatDate } from "./changeTimezone/changeFormat";
+export { calTimezone, convertTimezone } from "./changeTimezone/countryTimezone";
+export { getHolidays, isHoliday } from "./holiday/holiday";
